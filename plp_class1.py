748a4ad4ec4c4009efc082de0866a2fd9fb1a570
root <root@localhost.localdomain>
2023-01-27 21:54:37 +0300

    first class

---
+++ plp_class1.py
@@ -0,0 +1,6 @@
+"""so today was the first live session for the python power learn project and 
+i learnt how to save python files,install python in my device and also intergrated development 
+environment"""
+#we wrote and ran 
+print("Jambo Africa,welcome to the power learn platform")
+
